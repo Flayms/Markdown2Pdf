6f0763a3def29e02de42cae7f2f4462430b82744
Flayms <der.anton111@gmail.com>
2023-06-17 22:59:15 +0200

    + nuget package stuff

--- MarkdownToPdf/MarginOptions.cs
+++ MarkdownToPdf/MarginOptions.cs
@@ -1,8 +1,8 @@
 ﻿namespace MarkdownToPdf; 
 
 public class MarginOptions {
-  public string Top { get; set; }
-  public string Right { get; set; }
-  public string Bottom { get; set; }
-  public string Left { get; set; }
+  public string? Top { get; set; }
+  public string? Right { get; set; }
+  public string? Bottom { get; set; }
+  public string? Left { get; set; }
 }
