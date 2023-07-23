e06b341a795c49a6c096fd148b3f2c777fdc5ae2
Flayms <der.anton111@gmail.com>
2023-07-23 20:55:04 +0200

    ci: add script for conventional commits - automatic versioning

---
+++ .github/workflows/create-next-version.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# example-regex:          ^(\w.)+(\(.*\))?!*
+# breaking changes-regex: ^(\w.)+(\(.*\))?!:
+
+FILE_NAME=$1
+LAST_TAG=$(git describe --tags --abbrev=0)
+
+MAJOR_INDEX=0
+MINOR_INDEX=1
+PATCH_INDEX=2
+INDEX_TO_INCREASE=$MAJOR_INDEX
+
+echo "Last Tag: $LAST_TAG"
+
+# Create file
+git log $LAST_TAG..HEAD --no-merges --oneline > $FILE_NAME
+
+echo "Created file '$FILE_NAME' with following content:"
+echo "$(<$FILE_NAME)"git
+echo "----"
+
+# loop over each commit to determine new version
+while IFS="" read -r p || [ -n "$p" ]
+do
+  COMMIT_HASH=${p:0:6}
+  echo "parsing $COMMIT_HASH"
+
+  COMMIT_MSG=`git log --format=%B -n 1 $COMMIT_HASH`
+
+  # determine major changes
+  # todo: by conventional commits specification the BREAKING CHANGE is only allowed in the footer
+  if [[ "$COMMIT_MSG" == *"BREAKING CHANGES"* ]]; then
+    INDEX_TO_INCREASE=$MAJOR_INDEX
+    break
+  fi
+  
+  MSG_HEADER=`echo "${COMMIT_MSG}" | head -1`
+
+  # todo: check if this really works
+  # header containing '!' after the type/scope
+  if [[ "$MSG_HEADER" =~ ^(\w.)+(\(.*\))?!: ]]; then
+    INDEX_TO_INCREASE=$MAJOR_INDEX
+    break
+  fi
+
+  # todo: check if this really works
+  # determine minor changes
+  # header like 'feat[optional scope]:'
+  if [[ "$MSG_HEADER" =~ ^(feat)+(\(.*\))?!?: ]]; then
+    INDEX_TO_INCREASE=$MINOR_INDEX
+  fi
+
+  # don't need to increase patch because we always increase patch by default
+  #if [[ "$MSG_HEADER" =~ ^(fix)+(\(.*\))?!?: ]]; then
+  #  INDEX_TO_INCREASE=$MINOR_INDEX
+  #fi
+
+done < $FILE_NAME
+
+#----DEBUG----
+#LAST_TAG="v3.4.5"
+#INDEX_TO_INCREASE=2
+#-------------
+
+# remove v letter
+LAST_TAG_TRIMMED="${LAST_TAG:1}"
+MAJOR=$(echo $LAST_TAG_TRIMMED | awk -F \. {'print $1'})
+MINOR=$(echo $LAST_TAG_TRIMMED | awk -F \. {'print $2'})
+PATCH=$(echo $LAST_TAG_TRIMMED | awk -F \. {'print $3'})
+
+# increase major
+if [[ $INDEX_TO_INCREASE = $MAJOR_INDEX ]]; then
+  echo "increasing major"
+  let "MAJOR=MAJOR+1"
+  MINOR=0
+  PATCH=0
+fi
+
+# increase minor
+if [[ $INDEX_TO_INCREASE = $MINOR_INDEX ]]; then
+  echo "increasing minor"
+  let "MINOR=MINOR+1"
+  PATCH=0
+fi
+
+# increase patch
+if [[ $INDEX_TO_INCREASE = $PATCH_INDEX ]]; then
+  echo "increasing patch"
+  let "PATCH=PATCH+1"
+fi
+
+NEXT_TAG="v$MAJOR.$MINOR.$PATCH"
+
+# Informational Output
+echo "Next Tag: $NEXT_TAG"
