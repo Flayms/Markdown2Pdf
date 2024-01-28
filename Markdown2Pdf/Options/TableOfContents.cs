eada1d05966682a4302289aa3551b7ede107d425
Flayms <der.anton111@gmail.com>
2024-01-29 00:37:43 +0100

    feat(toc): options for ordered lists and indentation

--- Markdown2Pdf/Options/TableOfContents.cs
+++ Markdown2Pdf/Options/TableOfContents.cs
@@ -1,27 +1,46 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Markdown2Pdf.Options;
-public class TableOfContents(ListType listType) {
-  private readonly ListType _listType = listType;
+
+/// <inheritdoc cref="TableOfContents(bool, int)"/>
+public class TableOfContents {
+
+  private readonly int _maxIndentation;
+  private readonly bool _isOrdered;
   private const string _IDENTIFIER = "<!--TOC-->";
   private static readonly Regex _headerReg = new("^(?<depth>#{1,6}) +(?<title>.*)$", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.ExplicitCapture);
   private static readonly Regex _htmlElementReg = new("<[^>]*>[^>]*</[^>]*>|<[^>]*/>", RegexOptions.Compiled);
   private static readonly Regex _punctutationReg = new("[\\p{P} ]", RegexOptions.Compiled);
   private static readonly Regex _hyphenReg = new("-+", RegexOptions.Compiled);
 
-  public static TableOfContents BulletList => new(ListType.Bullet);
-  // TODO: public static TableOfContents NumberList => new(ListType.Number);
+  /// <summary>
+  /// Inserts a Table of Contents into the generated PDF. 
+  /// The TOC will be inserted into all <!--TOC--> comments within the markdown document. 
+  /// </summary>
+  /// <param name="isOrdered"></param>
+  /// <param name="maxIndentation"></param>
+  public TableOfContents(bool isOrdered = true, int maxIndentation = 3) {
+    if (maxIndentation < 1 || maxIndentation > 6)
+      throw new ArgumentOutOfRangeException();
+
+    this._isOrdered = isOrdered;
+    this._maxIndentation = maxIndentation;
+  }
 
   // TODO: tests
   internal void InsertInto(ref string markdownContent) {
     var matches = _headerReg.Matches(markdownContent);
     var tocBuilder = new StringBuilder();
+    var delimiter = _isOrdered ? "1. " : "* ";
 
     foreach (Match match in matches) {
-      var depth = match.Groups["depth"].Value.Length - 1; //subtract 1 because level 1 is at 0 depth
+      var depth = match.Groups["depth"].Value.Length;
+
+      if (depth > _maxIndentation)
+        continue;
+
       var title = match.Groups["title"].Value;
 
       // convert title to link
@@ -31,16 +50,11 @@ public class TableOfContents(ListType listType) {
       linkAddress = "#" + linkAddress.ToLower();
       var link = $"[{title}]({linkAddress})";
 
-      tocBuilder.Append(new string(' ', depth * 2));
-      tocBuilder.Append("* ");
+      tocBuilder.Append(new string(' ', (depth - 1) * 4)); // indent 4 spaces per level
+      tocBuilder.Append(delimiter);
       tocBuilder.AppendLine(link);
     }
     markdownContent = markdownContent.Replace(_IDENTIFIER, tocBuilder.ToString());
   }
 
-}
-
-public enum ListType {
-  Bullet,
-  Number,
 }
