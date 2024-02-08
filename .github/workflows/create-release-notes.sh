c2782c731606f21240a838287382db4fa5074907
Flayms <der.anton111@gmail.com>
2024-02-08 17:52:18 +0100

    ci: more beautiful changelog

--- .github/workflows/create-release-notes.sh
+++ .github/workflows/create-release-notes.sh
@@ -3,12 +3,40 @@
 OWNER_AND_REPO_NAME=$1
 CURRENT_TAG=$2
 FILE_NAME=$3
+declare -A SYMBOLS=(
+    [feat]=🚀
+    [feature]=🚀
+    [fix]=🐛
+    [bug]=🐛
+    [docs]=📚
+    [documentation]=📚
+    [style]=🎨
+    [refactor]=🔧
+    [refac]=🔧
+    [perf]=⚡
+    [test]=🔬
+    [build]=🏗
+    [ci]=🔧
+    [chore]=🧹
+    [revert]=⏪
+)
 
 LAST_TAG=$(git ls-remote --tags --sort=committerdate | grep -o 'v.*' | sort -r | head -1)
 
 echo "Creating $FILE_NAME"
 
-git log $LAST_TAG..HEAD --no-merges --oneline > $FILE_NAME
+log=$(git log $LAST_TAG..HEAD --no-merges --oneline)
+
+# add emojis
+for key in ${!SYMBOLS[@]}
+do
+  value=${SYMBOLS[$key]}
+
+  reg="^(\w{7}\s)($key(\((\w)+\))?:)((.)+)$"
+  log=$(echo "$log" | sed -r "s/$reg/\1$value\2\5/")
+done
+
+echo "$log" > $FILE_NAME
 echo "" >> $FILE_NAME
 echo "---" >> $FILE_NAME
 echo "**Full Changelog**: https://github.com/$OWNER_AND_REPO_NAME/compare/$LAST_TAG...$CURRENT_TAG" >> $FILE_NAME
