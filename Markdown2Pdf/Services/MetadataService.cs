ab8552582d71332c30299e9ec18db262bb32af6e
Flayms <anton.lange99@gmail.com>
2024-04-21 19:14:56 +0200

    feat(metadata): set pdf title from DocumentTitle

---
+++ Markdown2Pdf/Services/MetadataService.cs
@@ -0,0 +1,18 @@
+﻿using Markdown2Pdf.Options;
+
+namespace Markdown2Pdf.Services;
+internal class MetadataService {
+  private readonly Markdown2PdfOptions _options;
+  private readonly IConvertionEvents _events;
+
+  public MetadataService(Markdown2PdfOptions options, IConvertionEvents events) {
+    events.OnTemplateModelCreating += this._AddTitleToTemplate;
+    this._options = options;
+    this._events = events;
+  }
+
+  private void _AddTitleToTemplate(object sender, TemplateModelArgs e) {
+    var title = this._options.DocumentTitle ?? _events.OutputFileName;
+    e.TemplateModel.Add("title", title);
+  }
+}
